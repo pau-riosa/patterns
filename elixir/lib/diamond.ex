abe96168181dcd87c4088fb2256015845eb7ef49
pau <jethro.riosa@gmail.com>
2022-05-30 21:17:27 +0800

    update diamond and added test

--- elixir/lib/diamond.ex
+++ elixir/lib/diamond.ex
@@ -1,54 +1,50 @@
-pyramid = fn number ->
-  for i <- 1..number do
-    # for printing spaces
+defmodule Diamond do
+  def call(number) do
+    diamond = pyramid(number) ++ reverse_pyramid(number)
 
-    spaces =
-      for j when j <= number - i <- 1..number do
-        " "
-      end
+    diamond
+    |> Enum.into("", fn string ->
+      string = Enum.join(string)
+      "#{string}\n"
+    end)
+  end
 
-    # for printing star
-    star_number = 2 * i - 1
+  def pyramid(number) do
+    for i <- 1..number do
+      spaces =
+        for j when j <= number - i <- 1..number do
+          " "
+        end
 
-    star =
-      for _k <- 1..star_number do
-        "*"
-      end
+      star_number = 2 * i - 1
 
-    spaces ++ star
+      star =
+        for _k <- 1..star_number do
+          "*"
+        end
+
+      spaces ++ star
+    end
+  end
+
+  def reverse_pyramid(number) do
+    for i <- 1..number do
+      # for printing spaces
+
+      spaces =
+        for j when j < i <- 0..i do
+          " "
+        end
+
+      # for printing star
+      start_number = (number - i) * 2 - 1
+
+      star =
+        for k when k > 0 <- 0..start_number do
+          "*"
+        end
+
+      spaces ++ star
+    end
   end
 end
-
-reverse_pyramid = fn number ->
-  for i <- 1..number do
-    # for printing spaces
-
-    spaces =
-      for j when j < i <- 0..i do
-        " "
-      end
-
-    # for printing star
-    start_number = (number - i) * 2 - 1
-
-    star =
-      for k when k > 0 <- 0..start_number do
-        "*"
-      end
-
-    spaces ++ star
-  end
-end
-
-diamond = fn number ->
-  diamond = pyramid.(number) ++ reverse_pyramid.(number)
-
-  diamond
-  |> Enum.into("", fn string ->
-    string = Enum.join(string)
-    "#{string}\n"
-  end)
-end
-
-IO.puts(diamond.(10))
-IO.puts(diamond.(5))
