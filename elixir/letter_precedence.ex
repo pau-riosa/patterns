cc4800d25ecef7c33cb9c400f881789f2406443c
pau <jethro.riosa@gmail.com>
2022-05-30 20:57:04 +0800

    update letter precedence

--- elixir/letter_precedence.ex
+++ elixir/letter_precedence.ex
@@ -5,27 +5,26 @@ defmodule LetterPrecedence do
   Note: Each represented word contains a set of unique characters, i.e. the word does not contain duplicate letters.
   """
   def call(rules) do
-    split_letters = Enum.map(rules, &String.split(&1, ">"))
-    start_letters = Enum.map(split_letters, &Enum.at(&1, 0))
-    end_letters = Enum.map(split_letters, &Enum.at(&1, 1))
+    split_letters =
+      Enum.map(rules, fn rule ->
+        [first_letter, second_letter] = String.split(rule, ">")
+        {first_letter, second_letter}
+      end)
+
+    start_letters = Enum.map(split_letters, &elem(&1, 0))
+    end_letters = Enum.map(split_letters, &elem(&1, 1))
     start_letter = Enum.find(start_letters, &(not Enum.member?(end_letters, &1)))
 
-    split_letters
-    |> Enum.map(&{Enum.at(&1, 0), Enum.at(&1, 1)})
-    |> Enum.into(%{})
-    |> recur(start_letter, start_letter)
+    recur(split_letters, start_letter, start_letter)
     |> IO.inspect()
   end
 
-  def recur(map, _key, acc) when map_size(map) == 0 do
-    acc
-  end
-
-  def recur(map, key, acc) do
-    case Map.pop(map, key, :not_found) do
-      {:not_found, _} -> acc
-      {value, rest} -> recur(rest, value, acc <> value)
-    end
+  def recur(map, key, start_letter) do
+    Enum.reduce(map, start_letter, fn {k, v}, acc ->
+      if key == k,
+        do: recur(map, v, acc <> v),
+        else: acc
+    end)
   end
 end
 
@@ -37,6 +36,8 @@ end
   ["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"],
   ["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"],
   ["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"],
-  ["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]
+  ["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"],
+  ["I>N", "A>I", "P>A", "S>P"],
+  ["O>N", "Z>O", "N>E"]
 ]
 |> Enum.map(&LetterPrecedence.call(&1))
