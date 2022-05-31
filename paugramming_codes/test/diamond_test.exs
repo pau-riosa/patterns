1b30252dd3b33fe1ac6cb8ccbfa1a65f632611f7
pau <jethro.riosa@gmail.com>
2022-05-31 15:21:20 +0800

    remove paugramming-codes folder

--- paugramming_codes/test/diamond_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule DiamondTest do
-  use ExUnit.Case
-  alias Diamond
-
-  test "should print a diamond" do
-    assert "    *\n   ***\n  *****\n *******\n*********\n *******\n  *****\n   ***\n    *\n     \n" ==
-             Diamond.call(5)
-  end
-end
